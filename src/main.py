6728004e839e71a8a767047ec63659278265a061
Cam <cammclain@skiff.com>
2024-07-16 09:50:52 +0000

    add comments

--- src/main.py
+++ src/main.py
@@ -2,8 +2,12 @@
 from cryptcloud.application import create_litestar_app
 
 def main():
+    # Create an instance of the Litestar application
     app = create_litestar_app()
+    
+    # Run the application
     app.run()
+
 
 
 if __name__ == "__main__":
