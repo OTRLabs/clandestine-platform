7bf7c8f86acb4215a01bde3389cf34209a694489
Cam <cammclain@skiff.com>
2024-07-16 23:29:07 +0000

    clean up. mild refactor

---
+++ src/conti-platform/application/server/routers.py
@@ -0,0 +1,10 @@
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+
+
+
+
+# see https://github.com/litestar-org/litestar-fullstack/blob/main/src/app/server/routers.py for reference
