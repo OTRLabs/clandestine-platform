23926dd605dd066a1abc22157abbd8a2cdd16d8e
Cam <cammclain@skiff.com>
2024-07-16 23:49:34 +0000

    adjust the configs again.

--- src/conti-platform/application/config/base.py
+++ src/conti-platform/application/config/base.py
@@ -5,7 +5,7 @@ import os
 from dataclasses import dataclass, field
 from functools import lru_cache
 from pathlib import Path
-from typing import TYPE_CHECKING, Any, Final
+from typing import TYPE_CHECKING, Any
 
 from advanced_alchemy.utils.text import slugify
 from litestar.serialization import decode_json, encode_json
@@ -14,17 +14,19 @@ from litestar.utils.module_loader import module_to_os_path
 from sqlalchemy import event
 from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
 from sqlalchemy.pool import NullPool
-
+from .env_vars import DatabaseConfig, TorConfig
 if TYPE_CHECKING:
     from litestar.data_extractors import RequestExtractorField, ResponseExtractorField
 
-DEFAULT_MODULE_NAME = "app"
-BASE_DIR: Final[Path] = module_to_os_path(DEFAULT_MODULE_NAME)
-
-TRUE_VALUES = {"True", "true", "1", "yes", "Y", "T"}
 
 
 
 
 class Settings:
     
+    #app: str = "app"
+    database: DatabaseConfig = DatabaseConfig()
+    tor_service: TorConfig = TorConfig()
+    
+def get_settings() -> Settings:
+    return Settings()
--- src/conti-platform/application/config/const.py
+++ src/conti-platform/application/config/const.py
@@ -1,18 +1,24 @@
 from __future__ import annotations
 from pathlib import Path
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Final
 import binascii
 import json
 import os
-
+from litestar.utils.module_loader import module_to_os_path
 
 if TYPE_CHECKING:
     from litestar.data_extractors import RequestExtractorField, ResponseExtractorField
 
+
+DEFAULT_MODULE_NAME = "app"
+BASE_DIR: Final[Path] = module_to_os_path(DEFAULT_MODULE_NAME)
 DEFAULT_MODULE_NAME = "app"
 BASE_DIR: str = "/"
 TRUE_VALUES = {"True", "true", "1", "yes", "Y", "T"}
 ROOT_ROUTE: str = '/'
 APP_ROUTE: str = '/app'
 API_ROUTES: str = '/api'
+
+TRUE_VALUES = {"True", "true", "1", "yes", "Y", "T"}
+
 
