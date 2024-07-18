623f487bead8f115ee706033a4c83f27a2ebd73e
Cam <cammclain@skiff.com>
2024-07-18 12:19:15 +0000

    init some stuff. just general project structuring

---
+++ src/conti-platform/application/domain/auth/urls.py
@@ -0,0 +1,8 @@
+
+LOGIN_ROUTE: str = '/auth/login'
+REGISTER_ROUTE: str = '/auth/register'
+LOGOUT_ROUTE: str = '/auth/logout'
+REFRESH_TOKEN_ROUTE: str = '/auth/refresh-token'
+FORGOT_PASSWORD_ROUTE: str = '/auth/forgot-password'
+RESET_PASSWORD_ROUTE: str = '/auth/reset-password'
+SET_USER_ROLE_ROUTE: str = '/auth/set-user-role'
