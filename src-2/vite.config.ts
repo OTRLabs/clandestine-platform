6db3f506b22fd0b5671fbaa1a2199a66c4db6f05
Cam <cammclain@skiff.com>
2024-07-18 16:10:52 +0000

    rename conti-platform to app

--- src-2/vite.config.ts
+++ src-2/vite.config.ts
@@ -4,4 +4,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  resolve: {
+    alias: {
+      $components: 'src-2/src/components',
+    },
+  },
 })
