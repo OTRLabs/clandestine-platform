0ce09b9ca692df902aff13e3edd551a0f5376583
Cam <cammclain@skiff.com>
2024-07-18 17:00:03 +0000

    update svelte UI stuff. add first components

--- conti-platform/vite.config.ts
+++ conti-platform/vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
   resolve: {
     alias: {
-      $components: 'conti-platform/src/components',
+      $lib: path.resolve("./src/lib"),
     },
   },
 })
