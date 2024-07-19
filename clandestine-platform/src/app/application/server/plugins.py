fa75b337733e09a95c35838d7a84a0021064d929
Cam <cammclain@skiff.com>
2024-07-19 18:32:37 +0000

    refactor: Update plugins.py for application server configuration
    This commit refactors the plugins.py file in the application server to update the import statements for the config and builder modules. It also removes the unused GranianPlugin import and adds a comment suggesting the use of uvicorn instead. This change is made to improve the organization and maintainability of the codebase.

--- clandestine-platform/src/app/application/server/plugins.py
+++ clandestine-platform/src/app/application/server/plugins.py
@@ -4,12 +4,13 @@ from litestar_granian import GranianPlugin
 from litestar_saq import SAQPlugin
 from litestar_vite import VitePlugin
 
-from app.config import app as config
-from app.server.builder import ApplicationConfigurator
+from ..config import app as config
+from ..server.builder import ApplicationConfigurator
 
-structlog = StructlogPlugin(config=config.log)
-vite = VitePlugin(config=config.vite)
-saq = SAQPlugin(config=config.saq)
-alchemy = SQLAlchemyPlugin(config=config.alchemy)
-granian = GranianPlugin()
-app_config = ApplicationConfigurator()
+structlog: StructlogPlugin = StructlogPlugin(config=config.log)
+vite: VitePlugin = VitePlugin(config=config.vite)
+saq: SAQPlugin = SAQPlugin(config=config.saq)
+alchemy: SQLAlchemyPlugin = SQLAlchemyPlugin(config=config.alchemy)
+# granian: GranianPlugin = GranianPlugin() // use uvicorn instead of granian
+app_config: ApplicationConfigurator = ApplicationConfigurator()
+
