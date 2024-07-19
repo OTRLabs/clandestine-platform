9dcf983d871eed31b55b32df7678d5ff762b6ff3
Cam <cammclain@skiff.com>
2024-07-19 13:00:46 +0000

    feat: Add c2_implant_callback_url column to CommandNControlImplant model

--- conti-platform/src/app/application/database/models/c2_implant.py
+++ conti-platform/src/app/application/database/models/c2_implant.py
@@ -34,3 +34,4 @@ class CommandNControlImplant(UUIDAuditBase):
     c2_implant_tasks: Mapped[List[CommandNControlImplantTask]] = relationship("CommandNControlImplantTask", back_populates="c2_implant", cascade="all, delete-orphan")
     c2_listener: Mapped[CommandNControlListener] = relationship("CommandNControlListener", back_populates="c2_implant", uselist=False, lazy="joined")
     
+    
